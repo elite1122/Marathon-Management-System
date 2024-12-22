20d28f426dc85e616c47883c2953f30f0ad48629
elite <elitetuber1122@gmail.com>
2024-12-22 15:30:41 +0600

    SetUp Toast Container

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../shared/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MainLayout = () => {
     return (
         <div className="max-w-[1440px] mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     );
 };
